b84210fdd088efb2190e5e8fea01da448998c474
Marco André Mendes <marcoandre@gmail.com>
2025-04-11 15:12:57 -0300

    feat: criação da API para Livro.

--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,4 +1,6 @@
+from core.serializers import LivroSerializer
 from .user import UserViewSet
 from .autor import AutorViewSet
 from .categoria import CategoriaViewSet
 from .editora import EditoraViewSet
+from .livro import LivroViewSet
