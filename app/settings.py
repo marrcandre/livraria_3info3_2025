d26ba4b75f9ba8665323ce89c2ed743207ca67ef
Marco André Mendes <marcoandre@gmail.com>
2025-06-10 09:34:35 -0300

    feat: inclusão do DjangoModelPermissionsOrAnonReadOnly.

--- app/settings.py
+++ app/settings.py
@@ -147,8 +147,8 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     # "DEFAULT_AUTHENTICATION_CLASSES": ("core.authentication.TokenAuthentication",),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",
-        # "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        # "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
     ),
     'DEFAULT_PAGINATION_CLASS': 'app.pagination.CustomPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
