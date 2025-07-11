66d2ae845cf603172411c1cbc708c1c862abcad1
Marco André Mendes <marcoandre@gmail.com>
2025-07-11 13:40:31 -0300

    chore: script para atualizar livros via cURL.

--- scripts/atualiza_livros.sh
+++ scripts/atualiza_livros.sh
@@ -1,62 +1,46 @@
 #!/bin/bash
 
 # Livro 1
-curl -X PUT http://localhost:19003/api/livros/1/ \
+echo "Atualizando Livro ID 1"
+curl -X PATCH http://localhost:19003/api/livros/1/ \
   -H "Content-Type: application/json" \
   -d '{
-        "titulo": "O Código Limpo",
         "quantidade": 100,
-        "preco": "10.00",
-        "categoria": 1,
-        "editora": 1,
-        "autores": [1]
+        "preco": "10.00"
       }'
 
 # Livro 2
-curl -X PUT http://localhost:19003/api/livros/2/ \
+echo "Atualizando Livro ID 2"
+curl -X PATCH http://localhost:19003/api/livros/2/ \
   -H "Content-Type: application/json" \
   -d '{
-        "titulo": "O Codificador Limpo",
         "quantidade": 100,
-        "preco": "20.00",
-        "categoria": 1,
-        "editora": 1,
-        "autores": [1]
+        "preco": "20.00"
       }'
 
 # Livro 3
-curl -X PUT http://localhost:19003/api/livros/3/ \
+echo "Atualizando Livro ID 3"
+curl -X PATCH http://localhost:19003/api/livros/3/ \
   -H "Content-Type: application/json" \
   -d '{
-        "titulo": "As Crônicas de Nárnia",
-        "isbn": null,
         "quantidade": 100,
-        "preco": "30.00",
-        "categoria": 2,
-        "editora": 3,
-        "autores": [3]
+        "preco": "30.00"
       }'
 
 # Livro 4
-curl -X PUT http://localhost:19003/api/livros/4/ \
+echo "Atualizando Livro ID 4"
+curl -X PATCH http://localhost:19003/api/livros/4/ \
   -H "Content-Type: application/json" \
   -d '{
-        "titulo": "O Senhor dos Anéis",
         "quantidade": 100,
-        "preco": "40.00",
-        "categoria": 2,
-        "editora": 3,
-        "autores": [2]
+        "preco": "40.00"
       }'
 
 # Livro 5
-curl -X PUT http://localhost:19003/api/livros/5/ \
+echo "Atualizando Livro ID 5"
+curl -X PATCH http://localhost:19003/api/livros/5/ \
   -H "Content-Type: application/json" \
   -d '{
-        "titulo": "O Hobbit",
         "quantidade": 100,
-        "preco": "50.00",
-        "categoria": 2,
-        "editora": 10,
-        "autores": [2]
+        "preco": "50.00"
       }'
